0e1223378c0d16ed910f1d00da954857c68d018d
ElchinNasirov <nasirov.elchin@gmail.com>
2020-08-13 16:42:03 -0700

    Added some code for Day 1

--- src/item.py
+++ src/item.py
@@ -1,13 +1,13 @@
-Class Item:
-def __init__(self, name, description):
-    self.name = name
-    self.description = description
+class Item:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
 
-def __str__(self):
-    return f"{self.name}: {self.description}"
+    def __str__(self):
+        return f"{self.name}: {self.description}"
 
-def on_take(self):
-    print(f"You picked up a {self.name}!")
+    def on_take(self):
+        print(f"You picked up a {self.name}!")
 
-def on_drop(self):
-    print(f"You dropped a {self.name}!")
+    def on_drop(self):
+        print(f"You dropped a {self.name}!")
